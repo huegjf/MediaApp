4ca3c732585216a789f5b3f502017bc35ea9e898
huegjf <77868204+huegjf@users.noreply.github.com>
2021-01-22 18:55:50 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+var constraints = { video: { facingMode: "user" }, audio: false };
+
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function (stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function (error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}
+
+//Once we have a video stream to work with, we can program the button 
+//to grab a frame from the stream that we’ll use as our image output.
+cameraTrigger.onclick = function () {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+
+//Last, we’ll need to initiate the cameraStart function 
+//when the window is finished loading.That’ll look like this…
+window.addEventListener("load", cameraStart, false);
